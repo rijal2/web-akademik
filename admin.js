162e52be1be9ec4d13a6f077e430c4efd9b12c42
rijal2 <merentas5benua27@gmail.com>
2021-04-16 15:42:12 +0700

    Navbar Part 6 - Memberi efek dropdown pada Navbar

--- admin.js
+++ admin.js
@@ -1,3 +1,11 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-  })
+  });
+
+  $('.feat-btn').click(function(){
+    $('div ul .feat-show').toggleClass("show");
+});
+
+$('.serv-btn').click(function(){
+  $('div ul .serv-show').toggleClass("show1");
+});
