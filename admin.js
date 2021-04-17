f5e5323143b6011f059f9de651fd2d730778dcd3
rijal2 <merentas5benua27@gmail.com>
2021-04-17 22:55:49 +0700

    Menambahkan badges pada masing-masing update menu di halaman dashboard

--- admin.js
+++ admin.js
@@ -4,8 +4,10 @@ $(function () {
 
   $('.feat-btn').click(function(){
     $('div ul .feat-show').toggleClass("show");
+    $('div ul .first').toggleClass("rotate");
 });
 
 $('.serv-btn').click(function(){
   $('div ul .serv-show').toggleClass("show1");
+  $('div ul .second').toggleClass("rotate");
 });
